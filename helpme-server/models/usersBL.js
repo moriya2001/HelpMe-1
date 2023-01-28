b98465ec7f999c77ab51c8cbd030e88cf11c6de9
moriya <moriyang19@gmail.com>
2023-01-28 22:42:43 +0200

    server

---
+++ helpme-server/models/usersBL.js
@@ -0,0 +1,18 @@
+const usersModel=require("./usersModel")
+const getUsers=()=>{
+    return new Promise((resolve,reject)=>{
+        usersModel.find({},(err,user)=>{
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve(user)
+            }
+        })
+    })
+}
+const createUser=(obj)=>{
+ return new Promise((resolve,reject)=>{
+    //  let user=new 
+ })
+}
