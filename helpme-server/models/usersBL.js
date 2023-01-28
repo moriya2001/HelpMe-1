2d9ed127aeb0c398ba487e6749ae1cf16c0686cb
moriya <moriyang19@gmail.com>
2023-01-29 00:16:35 +0200

    new

--- helpme-server/models/usersBL.js
+++ helpme-server/models/usersBL.js
@@ -1,7 +1,7 @@
 const usersModel=require("./usersModel")
 const getUsers=()=>{
     return new Promise((resolve,reject)=>{
-        
+
         usersModel.find({},(err,user)=>{
             if(err){
                 reject(err)
@@ -14,6 +14,39 @@ const getUsers=()=>{
 }
 const createUser=(obj)=>{
  return new Promise((resolve,reject)=>{
-    //  let user=new 
+     let user=new usersModel(obj)
+     user.save((err,data)=>{
+         if(err){
+             reject(err)
+         }
+         else{
+             resolve(data)
+         }
+     })
  })
 }
+const updateUser=(id,obj)=>{
+    return new Promise((resolve,reject)=>{
+        usersModel.findByIdAndUpdate(id,obj,(err)=>{
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve("update!!!")
+            }
+        })
+    })
+}
+const deleteUser=(id)=>{
+    return new Promise((resolve,reject)=>{
+        usersModel.findByIdAndDelete(id,(err)=>{
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve("delete!!!")
+            }
+        })
+    })
+}
+module.exports = { getUsers, createUser,updateUser,deleteUser }
