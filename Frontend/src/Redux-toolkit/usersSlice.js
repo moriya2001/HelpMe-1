d5d69713d20a6760d5260c20e0c46a2e0bdb7f70
moriya2001 <109893302+moriya2001@users.noreply.github.com>
2023-06-25 17:48:02 +0300

    updateUserSlice

--- Frontend/src/Redux-toolkit/usersSlice.js
+++ Frontend/src/Redux-toolkit/usersSlice.js
@@ -4,20 +4,23 @@ const usersSlice = createSlice({
   name: 'users',
   initialState: {
     currentUser: {
-      Id: '',
-      IsAdmin: false
+      Tz: '123'
     }
   },
   reducers: {
     setCurrentUser(state, action) {
       console.log(action.payload)
       state.currentUser = action.payload;
-    }
+      console.log(state.currentUser)
+    },
+    // getCurrentUser(state, action) {
+    //   return state.currentUser;
+    // },
   }
 })
-
+// export const selectCurrentUser = (state) => {
+//   return state.currentUser
+// }
 export const { setCurrentUser } = usersSlice.actions;
 export default usersSlice.reducer;
-export const selectUserId = (state) => {
-  return state.users.currentUser.Id
-}
+
